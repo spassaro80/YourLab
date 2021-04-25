ac8eea241842d2029d875785c6e0080213e21563
Stefano Passaro <stefano.passaro@gmail.com>
2021-04-25 08:48:14 +0200

    settings.py of pythonAnywhere

--- calcioStat/settings.py
+++ calcioStat/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('ENVIRONMENT') == "DEVELOPMENT")
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','YourLab.eu.pythonanywhere.com']
 
 
 # Application definition
